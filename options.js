4b99cf14c300e6b68fd6bc25609718e7e7915d82
Fabio Dassan <fabiodassan@gmail.com>
2025-03-26 10:50:39 -0300

    Hide edit option for existing timezones
    There is a bug in the edit function for the existing time zones and, for
    the sake of time, this feature is being disabled - further investigation
    needed.

--- options.js
+++ options.js
@@ -198,12 +198,12 @@ document.addEventListener('DOMContentLoaded', function() {
       zoneActions.className = 'zone-actions';
 
       // Create edit button
-      const editButton = document.createElement('button');
+      /*const editButton = document.createElement('button');
       editButton.className = 'edit-button';
       editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>';
       editButton.addEventListener('click', function() {
         editTimeZone(index);
-      });
+      });*/
 
       // Create delete button
       const deleteButton = document.createElement('button');
@@ -213,7 +213,7 @@ document.addEventListener('DOMContentLoaded', function() {
         deleteTimeZone(index);
       });
 
-      zoneActions.appendChild(editButton);
+      //zoneActions.appendChild(editButton);
       zoneActions.appendChild(deleteButton);
 
       zoneElement.appendChild(colorPreview);
